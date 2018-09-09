f74e0c1244da1def09347f41fde55e0f87bab28f
Luis Filipe Forra <forra.luis@me.com>
2018-09-09 23:30:04 +0100

    State added to the App with two hardcoded todos. Added an Todo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import Todos from './Todos';
 
 class App extends Component {
+  
+  state = {
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'buy some milk' }
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos
+    });
+
+  }
+  
   render() {
     return (
-      <div className="App">
+      <div className="todo-app container ">
         
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } />
+
       </div>
     );
   }
